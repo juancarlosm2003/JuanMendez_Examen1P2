beb5dcb0b0fcb505750dcffdc6b826f1c120a0d6
juancarlosm2003 <juancarlosmendezelias200@gmail.com>
2022-05-20 14:17:16 -0600

    subiendo repositorio

---
+++ src/juanmendez_examen1p2/JuanMendez_Examen1P2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juanmendez_examen1p2;
+
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juan Carlos Mendez
+ */
+public class JuanMendez_Examen1P2 {
+
+    static Scanner lea = new Scanner(System.in);
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int opcion = 0;
+        while (opcion != 3) {
+            System.out.println("administrar redes en C++, (JAVA) XD");
+            System.out.println("1.- Agregar PC");
+            System.out.println("2.- Ingresar a PC");
+            opcion = lea.nextInt();
+            if (opcion == 1){
+                System.out.println("Ingrese la direccion IP");
+                String Ip = lea.next();
+                System.out.println("Ingrese la mascara de red");
+                String mascara = lea.next();
+                
+            }
+        }
+    }
+}
---
+++ src/juanmendez_examen1p2/PCS.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juanmendez_examen1p2;
+
+/**
+ *
+ * @author Juan Carlos Mendez
+ */
+public class PCS {
+    private String direccion, mascara, hostname;
+
+    public PCS() {
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getMascara() {
+        return mascara;
+    }
+
+    public void setMascara(String mascara) {
+        this.mascara = mascara;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    @Override
+    public String toString() {
+        return "PCS{" + "direccion=" + direccion + ", mascara=" + mascara + ", hostname=" + hostname + '}';
+    }
+    
+    
+}
